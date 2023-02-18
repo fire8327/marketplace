5c8ca43d5cb513bc4838cb28ba8477f4627812c0
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-02-18 13:28:51 +0000

    create cart

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -24,6 +24,30 @@ overlay.addEventListener("click", () => {
   body.classList.remove("overflow-hidden"); 
 });
 
+/* cart */
+/*jshint esnext: true */
+const toggler2 = document.getElementById("toggler2");
+const mNav2 = document.getElementById("mobile__nav2");
+const close2 = document.getElementById("toggler__expanded2");
+
+toggler2.addEventListener("click", () => {
+  mNav2.classList.remove("translate-x-full");
+  overlay.classList.remove("hidden");
+  body.classList.add("overflow-hidden");
+});
+
+close2.addEventListener("click", () => {
+  mNav2.classList.add("translate-x-full");
+  overlay.classList.add("hidden");
+  body.classList.remove("overflow-hidden"); 
+});
+
+overlay.addEventListener("click", () => {
+  mNav2.classList.add("translate-x-full");
+  overlay.classList.add("hidden"); 
+  body.classList.remove("overflow-hidden"); 
+});
+
 /* range */
 /* range */
 window.onload = function(){
