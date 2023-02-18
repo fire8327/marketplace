cad579736f9f43edbfa68539881341b6d67caa37
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-02-18 13:50:14 +0000

    create sign in/up

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -48,34 +48,4 @@ overlay.addEventListener("click", () => {
   body.classList.remove("overflow-hidden"); 
 });
 
-/* range */
-/* range */
-window.onload = function(){
-  slideOne();
-  slideTwo();
-};
-
-let first = document.getElementById("slider-1");
-let second = document.getElementById("slider-2");
-let min = document.getElementById("range-1");
-let max = document.getElementById("range-2");
-let minGap = 0;
-
-function slideOne(){
-  if(parseInt(second.value) - parseInt(first.value) <= minGap){
-      first.value = parseInt(second.value) - minGap;
-  }
-  min.value = first.value;
-  min.dataset.from = first.value;
-  fillColor();    
-}
-function slideTwo(){
-  if(parseInt(second.value) - parseInt(first.value) <= minGap){
-      second.value = parseInt(first.value) + minGap;
-  }
-  max.value = second.value;
-  max.dataset.before = second.value;
-  fillColor();
-}
-
 
